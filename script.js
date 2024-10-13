fb41cdabb4ed90fc2f56feec90a5fe30bef541b3
rhk998 <130037816+rhk998@users.noreply.github.com>
2024-10-13 19:07:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const charCount = {};
+
+	for(let char of str){
+		charCount[char] = (charCount[char] || 0) + 1;
+	}
+
+	for (let char of str) {
+        if (charCount[char] === 1) {
+            return char; // Return the first non-repeated character
+        }
+    }
+
+    return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
